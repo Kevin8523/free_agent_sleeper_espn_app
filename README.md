8849a89ec0a2accbc4f43c41da1c4e670d817ae1
Kevin Huang <Kevin8523@gmail.com>
2018-09-06 21:46:11 -0500

    Finished script - 1st iteration

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - Install webdriver for your internet browser, this program for Firefox (http://learn-automation.com/firefox-browser-on-mac-using-selenium-webdriver/)
 - Create excel worksheet prior to the very first run if you want to keep an record keeping
 
-## Program Requirement
+## Use config.yaml to add in all the information needed: Script Requirement
 - ESPN username: Username@gmail.com
 - ESPN Password: password_here
 - League ID URL: http://games.espn.com/ffl/leagueoffice?leagueId=LEAGUE_ID_NUMBER_WILL_BE_HERE
--- main.py
+++ main.py
@@ -17,35 +17,68 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 import time
 
-# Reading Excel Spreadsheets
-import openpyxl
-import os
-
 # Error Reading
 import sys
+import os
 
 # Keep User Pass Hidden
 import getpass
 
+# Config file
+import yaml
+
+###################################################
+# Load config file and seperate for convenience
+###################################################
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# os.chdir('/Users/kevin8523/Desktop/Github/waiver_espn_app')
+with open('test_config.yaml', 'rt') as f:
+    config = yaml.load(f)
+    
+espn_config = config['espn_config']
+driver_path_config = config['driver_path_config']
+add_player_config = config['add_player_config']
+drop_position_config = config['drop_position_config']
 
 ###################################################
 # Input ESPN User Information
 ###################################################
-# For best results hardcode information and comment out the inputs
-my_username = input("Please input ESPN username: ")
-my_password = getpass.getpass('Password:')
-my_league = input("Please input ESPN username: ")
-# my_username = 'myusername@gmail.com' # Email or username
-# my_password = 'my_password'
-# my_league = 'http://games.espn.com/ffl/leagueoffice?leagueId=LEAGUE_ID_NUMBER_WILL_BE_HERE'
+# For best results put information in config.yaml
+my_username = '{username}'.format(**espn_config) # username
+my_password = '{pw}'.format(**espn_config) # pw
+my_league = '{league}'.format(**espn_config)
+# my_username = input("Please input ESPN username: ")
+# my_password = getpass.getpass('Password:')
+# my_league = input("Please input ESPN league: ")
+
+
+###################################################
+# Dictionary for ESPN roster free agent drop
+###################################################
+position = {
+        'QB': '6',
+        'RB1': '7',
+        'RB2': '8',
+        'WR1': '9',
+        'WR2': '10',
+        'TE': '11',
+        'FLEX': '12',
+        'D/ST': '13',
+        'K': '14',
+        'BENCH1': '17',
+        'BENCH2': '18',
+        'BENCH3': '19',
+        'BENCH4': '20',
+        'BENCH5': '21'
+        }
 
 
 ###################################################
 # Script to Run Program 
 ###################################################
 # Path of the driver you installed
-path = '/Users/kevin8523/Desktop/Github/waiver_espn_app/'
-filename = 'geckodriver'
+path = '{filepath}'.format(**driver_path_config)
+filename = '{file}'.format(**driver_path_config)
 firefox_path = f'{path}{filename}'
 
 # Opens Firefox & goes to the Website (Must download chrome driver from the interwebs)
@@ -73,7 +106,7 @@ browser.find_element_by_xpath('//*[@id="games-tabs"]/li[3]/a').click() # Goes to
 
 # Add Free Agent
 free_agent = browser.find_element_by_id('lastNameInput')
-free_agent.send_keys('Kai Forbath')
+free_agent.send_keys('{add_player}'.format(**add_player_config))
 browser.find_element_by_id('lastNameSubmit').click()
 time.sleep(8)
 
@@ -82,11 +115,12 @@ add_player = browser.find_element_by_class_name('addButton')
 add_player.click()
 
 # Selects the player to remove
-# Remove player based on position
-# 6-14 are QB - K >> 6 = QB, 3 = RB, 14 = K
-# 17-21 are Bench: 17 = 1st bench, 18 = 2nd bench player
-# Add an index to have numbers correspond to each position
-drop_player_position = '21'
+# Input Options: 
+# QB,RB1,RB2,WR1,WR2,TE,FLEX,D/ST,K,BENCH1,BENCH2,BENCH3,BENCH4,BENCH5
+# BENCH1 is the first bench player from the top, BENCH5 is the bottom most bench player
+position_input = '{position_drop}'.format(**drop_position_config)
+
+drop_player_position = position[position_input]
 drop_player_xpath ='/html/body/div[2]/table/tbody/tr/td/div[3]/div/div/div/div[4]/div/div/div/div[3]/form/table[2]/tbody/tr/td/table/tbody/tr['+ drop_player_position +']/td[1]/input'
 remove_player = browser.find_element_by_xpath(drop_player_xpath)
 remove_player.click()
@@ -95,5 +129,24 @@ remove_player.click()
 browser.find_element_by_xpath('/html/body/div[2]/table/tbody/tr/td/div[3]/div/div/div/div[4]/div/div/div/div[3]/form/div/input[1]').click()
 
 # Confirm submit
-# uncomment below to have it run completely
 browser.find_element_by_xpath('/html/body/div[2]/table/tbody/tr/td/div[3]/div/div/div/div[4]/div/div/div/form/div/input[1]').click()
+
+
+"""
+# Next Steps 
+2. Check if player is available to add
+6. Write function to loop over
+8. Make a log file
+"""
+
+
+
+
+###################################################
+# Send Alert Email
+###################################################
+"""
+1. Logic to only do Create request
+2. Pull email and input text
+3. Send
+"""
